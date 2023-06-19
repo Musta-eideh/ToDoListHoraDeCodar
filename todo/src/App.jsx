5192b2e1cdc4c36c0e4a366f2657b9e950ee1651
Musta-eideh <mustafaeideh100@outlook.com>
2023-06-18 21:55:02 -0300

    Estrutura da página first real commit

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -2,11 +2,49 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // Colocar os dados vamos utilizar setTodos
+  // Consultar os dados vamos utilizar o todos
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text: "Criar funcionalidade X no sistema",
+      category:"Trabalho",
+      isCompleted: false,
+    },
+    {
+      id:2,
+      text: "Ir para a academia",
+      category:"Pessoal",
+      isCompleted: false,
+    },
+    {
+      id:3,
+      text: "Estudar React",
+      category:"Estudos",
+      isCompleted: false,
+    },
+  ]);
 
   return (
-    <div>
-      Olá react!
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {/* Exibindo todos os Todo com map*/}
+        {todos.map((todo) => (
+          <div className="todo">
+            <div className="content">
+              {/* Imprimindo o conteudo que está no objeto todo(linha 35) */}
+              <p>{todo.text}</p>
+              <p>({todo.category})</p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>x</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
