855bdb6ef07bdc2e0093fac396968a9aa660af1d
Musta-eideh <mustafaeideh100@outlook.com>
2023-06-22 21:40:04 -0300

    Ordem alfabetica  e finalizando projeto

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -68,13 +68,18 @@ function App() {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search  search={search} setSearch={setSearch}/>
-      <Filter filter={filter} setFilter={setFilter} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
         {/* Exibindo todos os Todo com map*/}
         {todos
         .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
         .filter((todo) => 
           todo.text.toLowerCase().includes(search.toLowerCase())
+        )
+        .sort((a, b) => 
+          sort === "Asc" 
+          ? a.text.localeCompare(b.text) 
+          : b.text.localeCompare(a.text)
         )
         .map((todo) => (
           // Componente precisa funcionar passando a prop 'todo' presente na pasta Todo.jsx
